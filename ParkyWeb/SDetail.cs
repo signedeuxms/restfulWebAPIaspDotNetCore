3f41ac6af1a0e8f83ae64fa7cb7084be314542f7
signeMuriel <signedeuxms@yahoo.fr>
2020-05-15 11:25:26 -0400

    parky web: client side

---
+++ ParkyWeb/SDetail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyWeb
+{
+    public static class SDetail
+    {
+        public static string APIbaseUrl = "https://localhost:44315/";
+        public static string NationalParkAPIpath = APIbaseUrl + "api/v1/nationalparks";
+        public static string TrialAPIpath = APIbaseUrl + "api/v1/trails";
+    }
+}
